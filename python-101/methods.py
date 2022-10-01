dea68020da6ea1c5315365fbadfa6c5653b4e604
Sena Demirbaş <83577247+senademirbass@users.noreply.github.com>
2022-10-01 17:34:39 +0300

    Add files via upload

---
+++ python-101/methods.py
@@ -0,0 +1,15 @@
+from re import A, X
+
+yaz_gel = "sena_demirbas"
+
+#METHODS
+#dir() metodu elimizdeki veri tipinin üzerinde kullanılabilecek metotları gösterir.
+
+e= dir(yaz_gel)
+print(e)
+
+f= yaz_gel.capitalize()
+print(f)
+
+g= yaz_gel.title()
+print(g)
---
+++ python-101/strings.py
@@ -0,0 +1,20 @@
+from re import A, X
+
+#STRINGS
+
+yaz_gel = "sena_demirbas"
+
+a = yaz_gel.upper()
+print(a)
+
+b= a.islower()
+print(b)
+
+c= yaz_gel.replace("e","a")
+print(c)
+
+d= yaz_gel.strip("s")
+print(d)
+
+
+
